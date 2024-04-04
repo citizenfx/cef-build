5bd66190e1f3318b5c901c1dab368903335c2c0f
Vektor <whiteman.chr@hotmail.com>
2024-04-04 11:51:21 +0200

    readd proprietary_codecs parameter

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
-set GN_DEFINES=is_chrome_branded=false symbol_level=1 is_official_build=true use_thin_lto=false ffmpeg_branding=Chrome
+set GN_DEFINES=is_chrome_branded=false symbol_level=1 is_official_build=true use_thin_lto=false ffmpeg_branding=Chrome proprietary_codecs=true
 set GYP_MSVS_VERSION=2019
 python automate-git.py^
  --download-dir=..\chromium_git^
