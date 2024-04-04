476818eac7ba1dbc6869446a8c61e3ea71ed18a6
Vektor <whiteman.chr@hotmail.com>
2024-04-04 11:42:06 +0200

    simplify build script

--- build/build.bat
+++ build/build.bat
@@ -1,12 +1,10 @@
-set GN_DEFINES=is_chrome_branded=false symbol_level=1 is_official_build=true use_thin_lto=false proprietary_codecs=true ffmpeg_branding=Chrome enable_widevine=true
+set GN_DEFINES=is_chrome_branded=false symbol_level=1 is_official_build=true use_thin_lto=false ffmpeg_branding=Chrome
 set GYP_MSVS_VERSION=2019
 python automate-git.py^
- --url=https://github.com/citizenfx/cef.git^
- --checkout=cfx-m103^
- --download-dir=e:\code2\chromium_git^
- --depot-tools-dir=e:\code2\depot_tools^
+ --download-dir=..\chromium_git^
+ --depot-tools-dir=..\depot_tools^
  --minimal-distrib-only^
  --x64-build^
  --no-cef-update^
  --no-debug-build^
- --no-depot-tools-update
+ --no-depot-tools-update %*
